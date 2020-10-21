b5509179d332cabbabca4e65fe7cbc9ff2bccc4d
Minieyes Chen <Minieyes_Chen@novatek.com.tw>
2020-10-21 10:06:38 +0800

    update vifm path

--- vifm.bash
+++ vifm.bash
@@ -3,7 +3,7 @@ vifm()
     if [ -f ~/.vifm/lastdir ]; then
         rm ~/.vifm/lastdir
     fi
-    winpty /l/vifm-w64-se-0.11-binary/vifm.exe "$@"
+    winpty /d/minieyes_chen/software/vifm-w64-se-0.11-binary/vifm.exe "$@"
     if [ -f ~/.vifm/lastdir ]; then
         cd `cat ~/.vifm/lastdir`
 		rm ~/.vifm/lastdir

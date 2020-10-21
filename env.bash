4508a12d8231c442c85a17c0f45375debff7d02c
Minieyes Chen(陳怡哲) <Minieyes_Chen@novatek.com.tw>
2020-10-21 09:06:40 +0800

    nova specific settings commit

--- env.bash
+++ env.bash
@@ -1,1 +1,9 @@
 export PATH=$PATH:~/bin
+
+export PS1="
+\[\033[m\]|\[\033[1;35m\]\t\[\033[m\]|\[\e[1m\]\u\[\e[1;36m\]\[\033[m\]:\[\e[0m\]\[\e[1;32m\][\w]\[\e[0m\]
+> "
+
+export REPO=/g/IMAGE/MD/TL1_Private/Software/minieyes/repo
+export http_proxy=http://nvt02863:20Q3toolteam@172.20.10.210:80
+export SSH_MINILIBRARY_URL=ssh://git@git.novatek.com.tw:7999/md_tl1/minilibrary.git
--- fzf.bash
+++ fzf.bash
@@ -1,7 +1,50 @@
 _fzf_compgen_path() {
-	fd --hidden --follow --exclude ".git" --exclude ".svn" . "$1"
+	fd --hidden --follow --exclude ".git" --exclude ".svn" --exclude "__history" --exclude ".vscode" --exclude ".vs" . "$1"
 }
 _fzf_compgen_dir() {
-	fd --type d --hidden --follow --exclude ".git" --exclude ".svn" . "$1"
+	fd --type d --hidden --follow --exclude ".git" --exclude ".svn" --exclude "__history" --exclude ".vscode" --exclude ".vs" . "$1"
 }
+fav() {
+	local top_dirs=(
+		"/d/minieyes_chen//"
+		"/d/minieyes_chen/program//"
+		"/g/IMAGE/MD/TL/Software/minieyes//"
+		"/g/IMAGE/MD/TL1_Private/Software/minieyes//"
+	)
+	local fixed_dirs=(
+		"/g/MD/Projects/IVE"
+		"/g/MD/Projects/IVE/Software/IVE4主程式"
+		"/g/MD/工作報告/周報/TL/陳怡哲"
+		"/g/MD/FAE Related/China_FAE"
+		"/g/MD/FAE Related/China_FAE/Tool/IVE InstallPack_20171025"
+		"/g/簡報資料公用區/工程類"
+		"/d/minieyes_chen/doc/eDP"
+	)
 
+	local target_dir=$(
+		{
+			local IFS=:
+			for dir in ${top_dirs[*]}; do
+				echo "${dir}"
+			done
+			for dir in ${fixed_dirs[*]}; do
+				echo "${dir}"
+			done
+		} | fzf --query "$1"
+	)
+
+	if [[ ! -z ${target_dir} ]] && [[ ${top_dirs[*]} == *"${target_dir}"* ]]; then
+		target_dir=$(
+			{
+				cd "${target_dir}"
+				for dir in $(fd --max-depth 1 --type d); do
+					echo "${target_dir}/${dir}"
+				done
+			} | fzf --query "$1"
+		)
+	fi
+
+	if [[ ! -z ${target_dir} ]]; then
+		cd "${target_dir}"
+	fi
+}

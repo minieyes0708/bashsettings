fe7f038cd66f6caf0c4e3fed6618804d1a4e8b41
Minieyes Chen <Minieyes_Chen@novatek.com.tw>
2020-10-21 11:22:36 +0800

    should use PROMPT_COMMAND to update git info

--- env.bash
+++ env.bash
@@ -1,6 +1,6 @@
 export PATH=$PATH:~/bin
 
-function get_git_info {
+function get_git_branch_name {
 	branch_name=$(git branch 2>/dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
 
 	if [[ ! -z "$branch_name" ]]; then
@@ -9,10 +9,12 @@ function get_git_info {
 		echo ""
 	fi
 }
-
-export PS1="
-\[\033[m\]|\[\033[1;35m\]\t\[\033[m\]|\[\e[1m\]\u\[\e[1;36m\]\[\033[m\]:\[\e[0m\]\[\e[1;32m\][\w]\[\e[0m\]$(get_git_info)
+function prompt_func() {
+	PS1="
+\[\033[m\]|\[\033[1;35m\]\t\[\033[m\]|\[\e[1m\]\u\[\e[1;36m\]\[\033[m\]:\[\e[0m\]\[\e[1;32m\][\w]\[\e[0m\]$(get_git_branch_name)
 > "
+}
+PROMPT_COMMAND=prompt_func
 
 export REPO=/g/IMAGE/MD/TL1_Private/Software/minieyes/repo
 export http_proxy=http://nvt02863:20Q3toolteam@172.20.10.210:80

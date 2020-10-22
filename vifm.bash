e6a56027aa70690479cb0172480bb8b0ce302edd
minieyes <chenvey2@gmail.com>
2020-10-22 20:01:13 +0800

    dont use winpty anymore

--- vifm.bash
+++ vifm.bash
@@ -3,7 +3,7 @@ vifm()
     if [ -f ~/.vifm/lastdir ]; then
         rm ~/.vifm/lastdir
     fi
-    winpty /l/vifm-w64-se-0.11-binary/vifm.exe "$@"
+    /l/vifm-w64-se-0.11-binary/vifm.exe "$@"
     if [ -f ~/.vifm/lastdir ]; then
         cd `cat ~/.vifm/lastdir`
 		rm ~/.vifm/lastdir

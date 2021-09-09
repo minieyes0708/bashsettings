c35ca7a011db238fe22cea2f1ded77e68be29a66
Minieyes Chen <Minieyes_Chen@novatek.com.tw>
2021-09-09 09:40:34 +0800

    ignore tags file

--- main.bash
+++ main.bash
@@ -1,7 +1,7 @@
 if [ -d "$HOME/.bashrc.d" ]; then
     for file in $(ls $HOME/.bashrc.d); do
         fullfile="$HOME/.bashrc.d/$file"
-        if [ ! -d "$fullfile" ] && [ -r "$fullfile" ] && [ "$file" != 'main.bash' ]; then
+        if [ ! -d "$fullfile" ] && [ -r "$fullfile" ] && [ "$file" != 'main.bash' ] && [ "$file" != 'tags' ]; then
             . $fullfile
         fi
     done
